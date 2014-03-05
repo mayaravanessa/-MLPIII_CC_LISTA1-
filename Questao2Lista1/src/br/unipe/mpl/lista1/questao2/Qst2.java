3ee0c85495637b7f7004050151e4cb90d482764b
Mayh <Mayh@MayaraVanessa>
2014-03-05 14:48:02 -0300

    Questao 2

---
+++ Questao2Lista1/src/br/unipe/mpl/lista1/questao2/Qst2.java
@@ -0,0 +1,24 @@
+package br.unipe.mpl.lista1.questao2;
+
+public class Qst2 {
+	
+	/*
+	 * Um estado (atributos): São o conjunto de propriedades da classe. "Variáveis"
+	 * Exemplo: Nome, idade, sexo, cpf são atributos da classe Pessoa
+	 * 
+	 * Um comportamento(Métodos): É uma forma de pedir que o objeto faça algo, ou como 
+	 * ele deve se comportar em relação a cada problema.
+	 * Exemplo: Na classe Celular, o método é Ligar, desligar, receber chamada entre outros.
+	 * 
+	 * Interface(Assinatura): É o título do método que é composto pelo: Modificador de acesso,
+	 * tipo de retorno, o nome do método, e os parametros.
+	 * Exemplo: public void Ligar(){
+	 * 			Na assinatura não precisamos saber o corpo do método
+	 * }
+	 * 
+	 * Uma identidade: É a propriedade de um objeto que se diferencia de outros objetos.
+	 * Exemplo: Lâmpada: Cod de Fabricação 001, 
+	 * 					 Cod de Fabricação 002;
+	 */
+
+}
