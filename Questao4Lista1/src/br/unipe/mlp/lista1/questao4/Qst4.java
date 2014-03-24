3dce1a40ec29dfed75b24a080aee0e39121b9b69
mayaravanessa <mayaravanessa25@hotmail.com>
2014-03-23 22:50:41 -0300

    Qst 4

---
+++ Questao4Lista1/src/br/unipe/mlp/lista1/questao4/Qst4.java
@@ -0,0 +1,18 @@
+package br.unipe.mlp.lista1.questao4;
+
+/* Questão 4
+ * Feito por Diego Veloso & Mayara Vanessa - P4 - Ciência da Computação
+ */
+
+public class Qst4 {
+	
+	/* Polimorfismo(do grego "muitas formas") é a qualidade que permite que uma
+	interface acesse uma classe geral de ações.
+
+ 	Um exemplo simples de polimorfismo pode ser encontrado no volante de um automóvel.
+ 	O volante(isto é, a interface) é o mesmo não importando o tipo de mecanismo
+ 	de direção usado, isto é, o volante vai funcionar da mesma forma se seu carro tem
+ 	direção manual ou hidraúlica.
+   */
+
+}
